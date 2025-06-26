e8a52b33d3b962fb61adfda1107407d20bf65ef7
Adrian20064 <carrascoadrian2006@gmail.com>
2025-06-26 01:54:32 -0700

    DEV

--- assigment6/bitwise/forms.py
+++ assigment6/bitwise/forms.py
@@ -6,3 +6,6 @@ class NumberForm(forms.Form):
     c = forms.IntegerField(label='Enter third number', min_value=0)
     d = forms.IntegerField(label='Enter fourth number', min_value=0)
     e = forms.IntegerField(label='Enter fifth number', min_value=0)
+
+
+#DEV# 
